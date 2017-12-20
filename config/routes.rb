9cf7e41394523ee8dad11bc7f4cd699c9e60a0a5
JTong <eldertong@gmail.com>
2017-12-20 12:17:07 -0500

    form is created and showing the content of the form on submit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index' # 'posts#index' posts is the controller
   get 'about' => 'pages#about'
+  resources :posts # this adds routes viewable via rake routes in the command line
 end
