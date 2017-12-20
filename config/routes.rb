a9d875f92787db902d082e4004d32b7365bc1696
JTong <eldertong@gmail.com>
2017-12-20 14:04:59 -0500

    working on adding navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index' # 'posts#index' posts is the controller
-  get 'about' => 'pages#about'
+  root 'posts#index', as: 'home' # 'posts#index' posts is the controller
+  get 'about' => 'pages#about', as: 'about'
   resources :posts # this adds routes viewable via rake routes in the command line
 end
